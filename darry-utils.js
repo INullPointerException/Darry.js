56343ee588abfc1c17c7b4799827232d1fb6f26c
INullPointerException <1075549323@qq.com>
2018-05-19 23:44:59 +0800

    添加日期转化时间戳函数

--- darry-utils.js
+++ darry-utils.js
@@ -183,8 +183,22 @@
       }else{
         return false;
       }
-    }
+    },
 
+    timestampToTime: function(timestamp) {
+          //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+          if(timestamp.toString().length === 10){
+              timestamp *= 1000;
+          }
+          var date = new Date(timestamp);
+          Y = date.getFullYear() + '-';
+          M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+          D = date.getDate() + ' ';
+          h = date.getHours() + ':';
+          m = date.getMinutes() + ':';
+          s = date.getSeconds();
+          return Y+M+D+h+m+s;
+      }
   };
 
   window.DarryUtils = DarryUtils;
