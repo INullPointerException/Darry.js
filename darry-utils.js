047916a8f4b52d01d2c596c37c0d6cd945d536ca
INullPointerException <1075549323@qq.com>
2019-06-25 17:28:22 +0800

    添加变量类型判断函数types,并将darry-utils.js重新调整结构

--- darry-utils.js
+++ darry-utils.js
@@ -1,49 +1,85 @@
 (function(window, document) {
-  var DarryUtils = {
-
-    /**
-     * 出生年月校验
-     */
-    checkMonthDay: function(year, month, day) {
-      switch (month) {
-        case '01':
-        case '03':
-        case '05':
-        case '07':
-        case '08':
-        case '10':
-        case '12':
-          if (day > 31) {
-            return false;
-          }
+  var ArrayUtils = {
+    removeTextOfArray: function(array, removeText) {
+      var dx = -1;
+      for (var i = 0, n = 0; i < array.length; i++) {
+        if (array[i] === removeText) {
+          dx = i;
           break;
-        case '04':
-        case '06':
-        case '09':
-        case '11':
-          if (day > 30) {
-            return false;
-          }
-          break;
-        case '02':
-          if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            if (day > 29) {
-              return false;
-            }
-          } else {
-            if (day > 28) {
-              return false;
-            }
-          }
-          break;
-        default:
-          return false;
+        }
       }
-      return true;
-    },
+      if (dx === -1) {
+        return false;
+      }
+      array.splice(dx, 1);
+    }
+  };
 
+  var TimeUtils = {
+    /**
+     * 时间戳转化为时间格式
+     * @param {时间戳} timestamp
+     */
+    timestampToTime: function(timestamp) {
+      //时间戳为10位需*1000，时间戳为13位的话不需乘1000
+      if (timestamp.toString().length === 10) {
+        timestamp *= 1000;
+      }
+      var date = new Date(timestamp);
+      Y = date.getFullYear() + "-";
+      M =
+        (date.getMonth() + 1 < 10
+          ? "0" + (date.getMonth() + 1)
+          : date.getMonth() + 1) + "-";
+      D = date.getDate() + " ";
+      h = date.getHours() + ":";
+      m = date.getMinutes() + ":";
+      s = date.getSeconds();
+      return Y + M + D + h + m + s;
+    }
+  };
+
+  var CommonUtils = {
     //身份证号合法性校验
     IdentityCodeValid: function(sfzh) {
+      var checkMonthDay = function(year, month, day) {
+        switch (month) {
+          case "01":
+          case "03":
+          case "05":
+          case "07":
+          case "08":
+          case "10":
+          case "12":
+            if (day > 31) {
+              return false;
+            }
+            break;
+          case "04":
+          case "06":
+          case "09":
+          case "11":
+            if (day > 30) {
+              return false;
+            }
+            break;
+          case "02":
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+              if (day > 29) {
+                return false;
+              }
+            } else {
+              if (day > 28) {
+                return false;
+              }
+            }
+            break;
+          default:
+            return false;
+        }
+        return true;
+      };
+
       //是否为空
       if (sfzh == "") {
         return false;
@@ -51,7 +87,7 @@
 
       var pat18 = /^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|X)$/i;
       var pat15 = /^\d{8}(0[1-9]|1[12])(0[1-9]|[12]\d|3[01])\d{3}/;
-      if ((pat18.test(sfzh) || pat15.test(sfzh))) {
+      if (pat18.test(sfzh) || pat15.test(sfzh)) {
         //长度
         if (sfzh.length != 15 && sfzh.length != 18) {
           return false;
@@ -118,52 +154,21 @@
           var year = parseInt(sfzh.substr(6, 4));
           var month = sfzh.substr(10, 2);
           var day = parseInt(sfzh.substr(12, 2));
-          if (!this.checkMonthDay(year, month, day))
-            return false;
-          sfzh = sfzh.split('');
+          if (!this.checkMonthDay(year, month, day)) return false;
+          sfzh = sfzh.split("");
           //∑(ai×Wi)(mod 11)
           //加权因子
-          var factor = [
-            7,
-            9,
-            10,
-            5,
-            8,
-            4,
-            2,
-            1,
-            6,
-            3,
-            7,
-            9,
-            10,
-            5,
-            8,
-            4,
-            2
-          ];
+          var factor = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
           //校验位
           /*校验位规则
-                  公式:∑(ai×Wi)(mod 11)……………………………………(1)
-                 公式(1)中：
-      ai----表示第i位置上的号码字符值；
-      Wi----示第i位置上的加权因子，其数值依据公式Wi=2^(n-1）(mod 11)计算得出。
-      Wi 7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2 1
-      */
+              公式:∑(ai×Wi)(mod 11)……………………………………(1)
+             公式(1)中：
+  ai----表示第i位置上的号码字符值；
+  Wi----示第i位置上的加权因子，其数值依据公式Wi=2^(n-1）(mod 11)计算得出。
+  Wi 7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2 1
+  */
           //余数[ 0   1  2   3  4  5  6  7  8  9  10]
-          var parity = [
-            1,
-            0,
-            'X',
-            9,
-            8,
-            7,
-            6,
-            5,
-            4,
-            3,
-            2
-          ];
+          var parity = [1, 0, "X", 9, 8, 7, 6, 5, 4, 3, 2];
           var sum = 0;
           var ai = 0;
           var wi = 0;
@@ -180,50 +185,47 @@
           }
           return true;
         }
-      }else{
+      } else {
         return false;
       }
     },
     /*获取光标选中的文本 */
-    getSelectedText: function(){
-      var txt = '';
-      if(document.selection){
-          txt = document.selection.createRange().text;//ie
-      }else{
-          txt = document.getSelection();
+    getSelectedText: function() {
+      var txt = "";
+      if (document.selection) {
+        txt = document.selection.createRange().text; //ie
+      } else {
+        txt = document.getSelection();
       }
       return txt.toString();
     },
-    
-    timestampToTime: function(timestamp) {
-          //时间戳为10位需*1000，时间戳为13位的话不需乘1000
-          if(timestamp.toString().length === 10){
-              timestamp *= 1000;
-          }
-          var date = new Date(timestamp);
-          Y = date.getFullYear() + '-';
-          M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-          D = date.getDate() + ' ';
-          h = date.getHours() + ':';
-          m = date.getMinutes() + ':';
-          s = date.getSeconds();
-          return Y+M+D+h+m+s;
-      },
 
-      removeTextOfArray: function (array, removeText) {
-        var dx = -1;
-        for (var i = 0, n = 0; i < array.length; i++) {
-          if(array[i] === removeText){
-            dx = i;
-            break;
-          }
-        }
-        if(dx === -1){
-          return false;
-        }
-        array.splice(dx,1);
+    /**
+     * 变量类型判断
+     * Types.isString()
+     */
+    Types: (function() {
+      var Type = {};
+      var types = ["String", "Array", "Number"];
+      for (var i = 0; i < types.length ; i++ ) {
+        (function(type) {
+          Type["is" + type] = function(obj) {
+            return (
+              Object.prototype.toString.call(obj) == "[object "+ type +"]"
+            );
+          };
+        })(types[i]);
       }
+      return Type;
+    })()
+  };
+
+
+  var DarryUtils = {
+    common: CommonUtils,
+    time: TimeUtils,
+    array: ArrayUtils
   };
 
   window.DarryUtils = DarryUtils;
-}(window, document));
+})(window, document);
