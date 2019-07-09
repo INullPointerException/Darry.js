c3d2a0776a1af78527f0984b6dfc8f923b100912
INullPointerException <1075549323@qq.com>
2019-07-09 20:16:42 +0800

    添加发布订阅对象Event

--- darry-utils.js
+++ darry-utils.js
@@ -1,6 +1,81 @@
 (function(window, document) {
   var ArrayUtils = {
-    
+    isArrayLike: function(o) {
+      if (
+        o && // o不是null、undefined等
+        typeof o === "object" && // o是对象
+        isFinite(o.length) && // o.length是有限数值
+        o.length >= 0 && // o.length为非负值
+        o.length === Math.floor(o.length) && // o.length是整数
+        o.length < 4294967296
+      )
+        // o.length < 2^32
+        return true;
+      else return false;
+    },
+
+    /**
+     * 遍历数组或者类数组对象
+     * @param {待遍历数组或者类数组对象} obj
+     * @param {遍历的回调函数} callback
+     */
+    each: function(obj, callback) {
+      var value,
+        i = 0,
+        length = obj.length,
+        isArray = this.isArrayLike(obj);
+
+      if (isArray) {
+        for (; i < length; i++) {
+          value = callback.call(obj[i], i, obj[i]);
+          if (value === false) {
+            break;
+          }
+        }
+      } else {
+        for (i in obj) {
+          value = callback.call(obj[i], i, obj[i]);
+          if (value === false) {
+            break;
+          }
+        }
+      }
+      return obj;
+    },
+
+    /**
+     * 反向遍历数组
+     * @param {待遍历数组或者类数组对象} obj
+     * @param {遍历的回调函数} callback
+     */
+    reverseEach: function(obj, callback) {
+      var value,
+        length = obj.length,
+        isArray = this.isArrayLike(obj);
+
+      if (isArray) {
+        for (var l = length - 1; l >= 0; l--) {
+          value = callback.call(obj[l], l, obj[l]);
+          if (value === false) {
+            break;
+          }
+        }
+      } else {
+        for (var i in obj) {
+          value = callback.call(obj[i], i, obj[i]);
+          if (value === false) {
+            break;
+          }
+        }
+      }
+      return obj;
+    },
+
+    /**
+     * 移除数组中的指定文本
+     * @param {待移除文本数组} array
+     * @param {待移除文本} removeText
+     */
     removeTextOfArray: function(array, removeText) {
       var dx = -1;
       for (var i = 0, n = 0; i < array.length; i++) {
@@ -15,7 +90,7 @@
       array.splice(dx, 1);
     },
 
-    isEqual: function( array1, array2 ) {
+    isEqual: function(array1, array2) {
       var Iterator = function(obj) {
         var current = 0;
 
@@ -43,8 +118,8 @@
         };
       };
 
-      var iterator1 = Iterator( array1 );
-      var iterator2 = Iterator( array2 );
+      var iterator1 = Iterator(array1);
+      var iterator2 = Iterator(array2);
 
       let isEqual = true;
       if (iterator1.getLength() !== iterator2.getLength()) {
@@ -235,6 +310,7 @@
         return false;
       }
     },
+
     /*获取光标选中的文本 */
     getSelectedText: function() {
       var txt = "";
@@ -365,7 +441,179 @@
         };
       }
       addEvent(elem, type, handler);
-    }
+    },
+
+    Event: (function() {
+      var global = this,
+        Event,
+        _default = "default"; // 默认的命名空间名称
+
+      Event = (function() {
+        var _listen,
+          _trigger,
+          _remove,
+          _splice = Array.prototype.splice,
+          _shift = Array.prototype.shift,
+          _unshift = Array.prototype.unshift,
+          namespaceCache = {}, // 命名空间缓存对象
+          _create;
+
+        each = function(arr, fn) {
+          // 用来遍历缓存函数列表和离线事件列表
+          var ret;
+          for (var i = 0, l = arr.length; i < l; i++) {
+            var n = arr[i];
+            ret = fn.call(n, i, n);
+          }
+          return ret; //   这里最终返回的是什么？
+        };
+
+        /**
+         * 事件订阅
+         */
+        _listen = function(key, fn, cache) {
+          // 针对缓存列表中没有的事件类型，先初始化事件列表为空数组
+          if (!cache[key]) {
+            cache[key] = [];
+          }
+          // 对于已经订阅的事件类型，向该订阅类型的事件缓存列表中添加缓存函数
+          cache[key].push(fn);
+        };
+
+        /**
+         * 事件订阅移除
+         */
+        _remove = function(key, cache, fn) {
+          if (cache[key]) {
+            if (fn) {
+              // 倒叙遍历移除， 防止splice导致数组遍历出现bug
+              for (var i = cache[key].length; i >= 0; i--) {
+                if (cache[key][i] === fn) {
+                  _splice.call(cache[key], i, 1);
+                }
+              }
+            } else {
+              // 如果没有传递要移除的函数， 则移除该类订阅类型的全部回调函数
+              cache[key] = [];
+            }
+          }
+        };
+
+        /**
+         * 事件分发函数, 触发事件
+         */
+        _trigger = function() {
+          var cache = _shift.call(arguments), // 函数缓存列表
+            key = _shift.call(arguments), // 订阅事件类型
+            args = arguments, // 参数列表
+            _self = this,
+            stack = cache[key]; // 某种事件的调用栈
+
+          if (!stack || !stack.length) {
+            return;
+          }
+
+          return each(stack, function() {
+            // 这里的this为某种类型事件缓存函数列表中的每一个函数
+            return this.apply(_self, args);
+          });
+        };
+
+        /**
+         * 命名空间创建函数
+         */
+        _namespace = function(namespace) {
+          var namespace = namespace || _default; // 默认的命名空间为default
+          var cache = {}, // 事件缓存对象，  存储每一类事件类型的回调函数
+            offlineStack = [], //离线事件
+            ret = {
+              // 每一个命名空间对应的数据结构
+              listen: function(key, fn, last) {
+                _listen(key, fn, cache);
+                if (offlineStack === null) {
+                  return;
+                }
+                if (last === "last") {
+                  offlineStack.length && offlineStack.pop()();
+                } else {
+                  each(offlineStack, function() {
+                    // 这里的this为离线事件中每一个事件的回调函数
+                    this();
+                  });
+                }
+
+                offlineStack = null;
+              },
+
+              /**
+               * 订阅某种类型的事件一次
+               * @param {订阅事件类型} key
+               * @param {接收通知的回调函数} fn
+               * @param {} last
+               */
+              one: function(key, fn, last) {
+                // 移除缓存中已经订阅的事件
+                _remove(key, cache);
+                this.listen(key, fn, last);
+              },
+
+              remove: function(key, fn) {
+                _remove(key, cache, fn);
+              },
+
+              /**
+               * trigger('click', 1)
+               */
+              trigger: function() {
+                var fn,
+                  args,
+                  _self = this;
+
+                _unshift.call(arguments, cache);
+                args = arguments;
+                fn = function() {
+                  return _trigger.apply(_self, args);
+                };
+
+                if (offlineStack) {
+                  return offlineStack.push(fn);
+                }
+                return fn();
+              }
+            };
+
+          /* 如果调用create函数时候的命名空间已经存在直接返回命名空间缓存对象中的命名空间，
+          否则初始化新的命名空间为上面的数据钢结构 */
+          return namespace
+            ? namespaceCache[namespace]
+              ? namespaceCache[namespace]
+              : (namespaceCache[namespace] = ret)
+            : ret;
+        };
+
+        return {
+          namespace: _namespace,
+          one: function(key, fn, last) {
+            var event = this.namespace();
+            event.one(key, fn, last);
+          },
+          remove: function(key, fn) {
+            var event = this.namespace();
+            event.remove(key, fn);
+          },
+          listen: function(key, fn, last) {
+            var event = this.namespace();
+            event.listen(key, fn, last);
+          },
+          trigger: function() {
+            var event = this.namespace();
+            event.trigger.apply(this, arguments);
+          }
+        };
+      })();
+
+      return Event;
+    })()
   };
 
   var DarryUtils = {
